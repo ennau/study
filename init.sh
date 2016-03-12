fd29ea30e8a3ff7a323c41cf46400bdf8cb46161
ennau <ennau1@ukr.net>
2016-03-12 14:02:35 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,16 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+#### Django
+
+cd /home/box/web/
+django-admin startproject ask
+cd /home/box/web/ask/
+python manage.py startapp qa
+
+cat > /home/box/web/ask/qa/views.py <<EOF
+from django.http import HttpResponse 
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+EOF
 
